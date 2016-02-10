1674164bb6b744edc8cf265403bb9ec4d31822ee
WesamMikhail <wesam@lorenum.com>
2016-02-10 15:07:46 +0100

    Errorhandler only register if env is NOT production

--- src/Errors/ErrorHandler.php
+++ src/Errors/ErrorHandler.php
@@ -19,53 +19,54 @@ use Pimple\Container;
  */
 class ErrorHandler{
     public static function registerErrorHandlers(Container $container){
-        set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-            throw new HTTPException_500;
-        });
+        if($container["configs"]->getConfigsByKey("globals")->environment == "production") {
+            set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+                throw new HTTPException_500;
+            });
 
-        set_exception_handler(function(Exception $e) use ($container){
-            $code = 500;
-            $status = "Internal Server Error";
-            $message = 'Please contact an administrator about this error';
+            set_exception_handler(function (Exception $e) use ($container) {
+                $code = 500;
+                $status = "Internal Server Error";
+                $message = 'Please contact an administrator about this error';
 
-            if($e instanceof HTTPException){
-                $code = $e->getCode();
-                $status = $e->getStatus();
-                $message = $e->getMessage();
-            }
-            else{
-                $message .= " - Internal Code: " . $e->getCode();
-            }
+                if ($e instanceof HTTPException) {
+                    $code = $e->getCode();
+                    $status = $e->getStatus();
+                    $message = $e->getMessage();
+                } else {
+                    $message .= " - Internal Code: " . $e->getCode();
+                }
 
-            $response = $container["response"];
-            $response->setProtocol($container["request"]->getProtocol());
-            $response->setCode($code);
-            $response->setStatus($status);
-            $response->setMessage($message);
-
-            if(isset($container["logger"])) {
-                $container["logger"]->addLog("Response", $response);
-                $container["logger"]->store(new LoggerFileStorage("logs"));
-            }
-            $response->send();
-        });
-
-        register_shutdown_function(function() use ($container){
-            $error = error_get_last();
-
-            if($error !== null) {
                 $response = $container["response"];
                 $response->setProtocol($container["request"]->getProtocol());
-                $response->setCode(500);
-                $response->setStatus("Internal Server Error");
-                $response->setMessage("Unexpected error caused a fatal shutdown. Please contact the server administrator about this problem");
+                $response->setCode($code);
+                $response->setStatus($status);
+                $response->setMessage($message);
 
-                if(isset($container["logger"])) {
+                if (isset($container["logger"])) {
                     $container["logger"]->addLog("Response", $response);
                     $container["logger"]->store(new LoggerFileStorage("logs"));
                 }
                 $response->send();
-            }
-        });
+            });
+
+            register_shutdown_function(function () use ($container) {
+                $error = error_get_last();
+
+                if ($error !== null) {
+                    $response = $container["response"];
+                    $response->setProtocol($container["request"]->getProtocol());
+                    $response->setCode(500);
+                    $response->setStatus("Internal Server Error");
+                    $response->setMessage("Unexpected error caused a fatal shutdown. Please contact the server administrator about this problem");
+
+                    if (isset($container["logger"])) {
+                        $container["logger"]->addLog("Response", $response);
+                        $container["logger"]->store(new LoggerFileStorage("logs"));
+                    }
+                    $response->send();
+                }
+            });
+        }
     }
 }
